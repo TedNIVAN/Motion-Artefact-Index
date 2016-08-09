a4461094d4d1a5de2cde31c4ed6f8bbde58f20f0
Ted NIVAN <tednivan@teds-mbp.partners.org>
2016-08-09 13:48:31 -0400

    ENH: submit job

---
+++ js/mai_clusterpost/submitmai.js
@@ -0,0 +1,77 @@
+var clusterpost = require("clusterpost-lib");
+var argv = require('minimist')(process.argv.slice(2));
+var inputfiles = [];
+
+const getConfigFile = function () {
+ try {
+   // Try to load the user's personal configuration file
+   return require(process.cwd() + '/conf.my.json');
+ } catch (e) {
+   // Else, read the default configuration file
+   return require(process.cwd() + '/conf.json');
+ }
+}
+
+var imageName = argv['f'];
+var threshold = argv['t'];
+
+if (imageName != undefined && threshold != undefined)
+{
+    console.error("usage: mai -f -t");
+    console.error("node submitmai.js -f image file name -t threshold");
+    Process.exit(1);
+}
+
+inputfiles.push(imageName);
+
+var conf = getConfigFile();
+
+var job = {
+        "executable": "mai",
+        "parameters": [
+            {
+                "flag": "",
+                "name": threshold //To modify
+            },
+            {
+                "flag": "",
+                "name": imageName
+            }
+        ],
+        "inputs": [
+            {
+                "name": imageName
+            }
+        ],
+        "outputs": [           
+            {
+                "type": "file",
+                "name": "mai.json"
+            },
+            {
+                "type": "file",
+                "name": "stdout.out"
+            },
+            {
+                "type": "file",
+                "name": "stderr.err"
+            }
+        ],
+        "type": "job",
+        "userEmail": conf.user.email
+    };
+
+clusterpost.setClusterPostServer(conf.url);
+
+clusterpost.userLogin(conf.user)
+.then(function(res){
+    return clusterpost.getExecutionServers();
+})
+.then(function(res){
+    job.executionserver = res[0].name; //Or select the computing grid where you want to submit your job.
+    return clusterpost.createAndSubmitJob(job, inputfiles)
+})
+.then(function(jobid){ 
+    console.log(jobid);//Job id of the task submitted
+})
+.catch(console.error)
